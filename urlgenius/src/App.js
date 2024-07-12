d380ae93acef624989b5217d67665c8813709da5
Joao Pedro <joaoribeiro@locus.ne>
2024-07-12 16:25:43 -0300

    versão 1

--- urlgenius/src/App.js
+++ urlgenius/src/App.js
@@ -1,53 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import api from './utils/api'; // Importe o arquivo de configuração do Axios
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  const [dadosDoBackend, setDadosDoBackend] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.get('api/exemplo/');
-        setDadosDoBackend(response.data);
-      } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Exemplo de integração React + Django
-        </p>
-        {dadosDoBackend ? (
-          <div>
-            <h2>Dados Recebidos do Backend:</h2>
-            <ul>
-              {dadosDoBackend.map((item) => (
-                <li key={item.id}>{item.nome}</li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>Carregando dados do backend...</p>
-        )}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ urlgenius/src/components/Login.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import api from '../utils/api';
+import { Container, Box, Typography, TextField, Button, Checkbox, FormControlLabel, Alert, Grid, Link, InputAdornment,IconButton } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import '../css/Login.css';
+import { grey } from '@mui/material/colors';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await api.post('/token/', {
+                username,
+                password,
+            });
+            // Salva os tokens no localStorage
+            localStorage.setItem('access_token', response.data.access);
+            localStorage.setItem('refresh_token', response.data.refresh);
+            console.log('Login successful');
+            // Redirecionar ou fazer alguma ação após login bem-sucedido
+        } catch (error) {
+            setError('Nome de usuário ou senha incorretos');
+            console.error('There was an error!', error);
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-left">
+                <div className="login-logo">Logo</div>
+                <h2>Bem Vindo</h2>
+                <p>Você está no URL Genius.</p>
+            </div>
+            <div className="login-right">
+                <form onSubmit={handleSubmit} className="login-form">
+                    <Typography component="h1" variant="h5" sx={{ textAlign: 'center', fontSize: '3rem' }}>
+                        Login URL Genius
+                    </Typography>
+                    <Typography component="p" variant="body1" mt={1} sx={{ textAlign: 'center', fontSize: '0.9rem', color: '#828181' }}>
+                        Por favor, insira suas credenciais.
+                    </Typography>
+                    {error && <Alert severity="error" sx={{ mt: 2 }}>{error}</Alert>}
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Usuário"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Senha"
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            autoComplete="current-password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    <div className="form-group">
+                        <input type="checkbox" id="remember" />
+                        <label htmlFor="remember">Lembrar de mim</label>
+                        <a href="/forgot-password" className="forgot-password">Esqueceu sua senha?</a>
+                    </div>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2, backgroundColor: '#00B4CB', '&:hover': { backgroundColor: '#0099A8' } }}
+                    >
+                        Entrar
+                    </Button>
+                    <p className="contact">Não tem uma conta? <a href="/contact">Entre em contato.</a></p>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
